fb8cada100dac39ae02340f6dfce91eaf008ac4b
SusandeepShakya <susandeepshakya@gmail.com>
2022-10-14 18:34:53 +0545

    blog Api Completed

--- routes/posts.js
+++ routes/posts.js
@@ -21,9 +21,16 @@ router.put("/:id", async (req, res) => {
         if (post.username === req.body.username) {
 
             try {
+                const updatedPost = await Post.findByIdAndUpdate(req.params.id, {
+                    $set: req.body,
+                }, { new: true })
+                res.status(200).json("Updated post")
             } catch (err) {
+                res.status(500).json(err)
 
             }
+        } else {
+            res.status(401).json("Update Only tour pos")
         }
     } catch (err) {
         res.status(500).json(err)
@@ -31,7 +38,55 @@ router.put("/:id", async (req, res) => {
 })
 
 //Delete The Post
+router.delete("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.username === req.body.username) {
 
+            try {
+                await post.delete();
+                res.status(200).json("Post has bee deleted");
+            } catch (err) {
+                res.status(500).json(err)
+
+            }
+        } else {
+            res.status(401).json("delete only your post");
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 //Get Post
+router.get("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//Get All Post
+router.get("/", async (req, res) => {
+    const username = req.query.user;
+    const catName = req.query.cat;
+
+    try {
+        let posts;
+        if(username){
+            posts = await Post.find({username : username})
+        }else if(catName){
+            posts = await Post.find({categories:{
+                $in:[catName]
+            }}) 
+        }else{
+            posts = await Post.find();
+        }
+        res.status(200).json(posts);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
